71e02ae3288d2e98533cd959b43b93a7a018a6ff
Mats Lexell <matslexell88@gmail.com>
2023-07-19 11:21:09 +0100

    hello

--- helloWorld.ts
+++ helloWorld.ts
@@ -1,1 +1,26 @@
 console.log('DEBUG_ Hello World!');
+
+type MyUnion =
+  | {
+      kind: 'a';
+      a: string;
+    }
+  | {
+      kind: 'b';
+      b: string;
+    }
+  | {
+      kind: 'c';
+      c: string;
+    };
+
+const derp = (myUnion: MyUnion): string => {
+  switch (myUnion.kind) {
+    case 'a':
+      return myUnion.a;
+    case 'b':
+      return myUnion.b;
+    case 'c':
+      return myUnion.c;
+  }
+};
